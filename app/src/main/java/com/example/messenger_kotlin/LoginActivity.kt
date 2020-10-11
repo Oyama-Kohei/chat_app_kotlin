e3d82bd9e60cf2cca25c64edb016aad83c1395e4
t2020029 <ooyama@tdc.co.jp>
2020-10-11 16:01:00 +0900

    Login画面の追加

---
+++ app/src/main/java/com/example/messenger_kotlin/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.messenger_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
